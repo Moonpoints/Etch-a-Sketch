4db057ea5f734f77ec902cb49bc5476e07c44777
Moon <208090489+Moonpoints@users.noreply.github.com>
2025-06-07 06:35:54 -0500

    welp, it is now an hour later after I said I was going to bed but I have a functioning color selector and eraser. Actually done this time, maybe.

--- java.js
+++ java.js
@@ -2,22 +2,54 @@ let grid = document.querySelector("#grid");
 let button = document.querySelector("#reset");
 let buttons = document.querySelector("#buttons");
 let color = document.querySelector("#color");
-let eraser = document.querySelector("#eraser")
+let eraser = document.querySelector("#eraser");
+let etch = document.querySelector("#etch")
 let userChoice = 16;
+let currentMode = "darken";
+let currentColor = "grey"
 
-cellListener = () =>{
-document.querySelectorAll('.cell').forEach((cell) => {
-cell.addEventListener("mouseover", (sketch) => {
-    let currentColor = window.getComputedStyle(cell).backgroundColor;
-    let newColor = darkenColor(currentColor, 0.1);
-    cell.style.backgroundColor = newColor;
+cellListener = () => {
+    document.querySelectorAll('.cell').forEach((cell) => {
+        cell.addEventListener("mouseover", () => {
+            if (currentMode === "color") {
+                cell.style.backgroundColor = currentColor;
+            } else if (currentMode === "eraser") {
+                cell.style.backgroundColor = "grey";
+            } else if (currentMode === "darken") {
+                let curr = window.getComputedStyle(cell).backgroundColor;
+                cell.style.backgroundColor = darkenColor(curr, 0.1);
+            }
+        });
+    }
+)};
+etch.addEventListener("click", () => {
+    currentMode = "darken";
+})
+color.addEventListener("click", () => {
+    document.getElementById("colorPicker").style.display = "block";
+    currentMode = "color";
 });
+
+eraser.addEventListener("click",() => {
+    currentMode = "eraser";
+    document.querySelector("#colorPicker").style.display = "none";
+})
+
+document.querySelector("#closeColorPicker").addEventListener("click", () => {
+    document.querySelector("#colorPicker").style.display = "none";
 });
+
+const updateColor = () => {
+    let r = document.querySelector("#rSlider").value;
+    let g = document.querySelector("#gSlider").value;
+    let b = document.querySelector("#bSlider").value;
+    currentColor = `rgb(${r}, ${g}, ${b})`;
+    document.querySelector("#colorPreview").style.backgroundColor = currentColor;
 };
 
-//getEraser = () =>{}
-
-
+["rSlider", "gSlider", "bSlider"].forEach(id => {
+    document.getElementById(id).addEventListener("input", updateColor);
+});
 
 firstGrid = () =>{
 for (let i = 0; i < userChoice * userChoice; i ++){
